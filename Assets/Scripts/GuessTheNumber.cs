26d046c1154b356ad3577516be3b307c7d8b4142
Sharri'a Sneed <frostellachery26@gmail.com>
2024-02-02 14:24:26 -0600

    Checks for correct answer
    The game checks if the answer was correct and tests for invalid inputs

--- Assets/Scripts/GuessTheNumber.cs
+++ Assets/Scripts/GuessTheNumber.cs
@@ -16,35 +16,72 @@ public class NewBehaviourScript : MonoBehaviour{
     private int userGuess;
     private int chancesLeft = 3;
 
-
+    private int min = 1;
+    private int max = 10;
 
     void Start(){
         GameSetup();
     }
 
-    // Update is called once per frame
-    void Update(){
-        
-    }
 
     void GameSetup(){
         //Sets up the starting values of multiple variables
         correctNumber = GetRandomNumber();
         chancesLeft = 3;
-        guessText.text = "Welcom to the guessing game. You have " + chancesLeft.ToString() + " chances  to guess a number between 1-10.";
+        guessText.text = "Welcome to the guessing game. You have " + chancesLeft.ToString() + " chances  to guess a number between 1-10.";
         textInput.text = "Enter A Number...";
+        Debug.Log("the answer is " + correctNumber.ToString());
     }
-
-    int GetRandomNumber(){
-        //gets a random number between 1 and 10
-        int num = Random.Range(1,10+1);
-        return num;
-    }
-
     public void SubmitGuess(){
         if (textInput != null && !(chancesLeft <= 0)){
             userGuess = int.Parse(textInput.text);
-            Debug.Log("Guess Has Been Saved");
+            //Debug.Log("Guess Has Been Saved");
+            chancesLeft -= 1;
+            isGuessInRange();
+            Debug.Log("Guesses Left: " + chancesLeft.ToString());
+        }
+    }
+    
+    //============Excess Functions=================//
+    public void ResetGame(){
+        GameSetup();
+    }
+        int GetRandomNumber(){
+        //gets a random number between 1 and 10
+        int num = Random.Range(min,max+1);
+        return num;
+    }
+    void isGuessInRange(){
+        //Checks if the number is out of Range
+        if (userGuess > max || userGuess < min){
+            guessText.text = "That number is not between 1-10. Try Again.";
+        }
+        //If the number is in range
+        else{
+            IsGuessCorrect();
+        }
+    }
+    void IsGuessCorrect(){
+        //If the guess matches the correct number
+        if (userGuess == correctNumber){
+            guessText.text = "You have guessed the correct number!!! Restart to play again.";
+            chancesLeft = 0;
+        }
+        //If the guess does not match the correct number
+        else{
+            switch (chancesLeft){
+                case 2:
+                    guessText.text = "Nope, try again. You have " + chancesLeft.ToString() + " chances left.";
+                    break;
+                case 1:
+                    guessText.text = "Uh, oh. You have " + chancesLeft.ToString() + " chance left";
+                    break;
+                default:
+                    guessText.text = "Your bad at guessing. The number was " + correctNumber.ToString() + ". Reset to try again.";
+                    break;
+            }
         }
     }
 }
+
+    
